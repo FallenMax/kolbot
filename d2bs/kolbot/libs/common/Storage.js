bc4a51418a19df1d347c81f30d76c92b77e4cd26
FallenMax <FallenMax@gmail.com>
2022-01-09 19:26:03 +0800

    storage: support plugy

--- d2bs/kolbot/libs/common/Storage.js
+++ d2bs/kolbot/libs/common/Storage.js
@@ -1,396 +1,455 @@
 /**
-*	@filename	Storage.js
-*	@author		McGod, kolton (small kolbot related edits)
-*	@desc		manage inventory, belt, stash, cube
-*/
+ *	@filename	Storage.js
+ *	@author		McGod, kolton (small kolbot related edits)
+ *	@desc		manage inventory, belt, stash, cube
+ */
 
 var Container = function (name, width, height, location) {
-	var h, w;
+  var h, w;
 
-	this.name = name;
-	this.width = width;
-	this.height = height;
-	this.location = location;
-	this.buffer = [];
-	this.itemList = [];
-	this.openPositions = this.height * this.width;
+  this.name = name;
+  this.width = width;
+  this.height = height;
+  this.location = location;
+  this.buffer = [];
+  this.itemList = [];
+  this.openPositions = this.height * this.width;
 
-	// Initalize the buffer array for use, set all as empty.
-	for (h = 0; h < this.height; h += 1) {
-		this.buffer.push([]);
+  // Initalize the buffer array for use, set all as empty.
+  for (h = 0; h < this.height; h += 1) {
+    this.buffer.push([]);
 
-		for (w = 0; w < this.width; w += 1) {
-			this.buffer[h][w] = 0;
-		}
-	}
+    for (w = 0; w < this.width; w += 1) {
+      this.buffer[h][w] = 0;
+    }
+  }
 
-	/* Container.Mark(item)
-	 *	Marks the item in the buffer, and adds it to the item list.
-	 */
-	this.Mark = function (item) {
-		var x, y;
+  /* Container.Mark(item)
+   *	Marks the item in the buffer, and adds it to the item list.
+   */
+  this.Mark = function (item) {
+    var x, y;
 
-		//Make sure it is in this container.
-		if (item.location !== this.location || (item.mode !== 0 && item.mode !== 2)) {
-			return false;
-		}
+    //Make sure it is in this container.
+    if (
+      item.location !== this.location ||
+      (item.mode !== 0 && item.mode !== 2)
+    ) {
+      return false;
+    }
 
-		//Mark item in buffer.
-		for (x = item.x; x < (item.x + item.sizex); x += 1) {
-			for (y = item.y; y < (item.y + item.sizey); y += 1) {
-				this.buffer[y][x] = this.itemList.length + 1;
-				this.openPositions -= 1;
-			}
-		}
+    //Mark item in buffer.
+    for (x = item.x; x < item.x + item.sizex; x += 1) {
+      for (y = item.y; y < item.y + item.sizey; y += 1) {
+        this.buffer[y][x] = this.itemList.length + 1;
+        this.openPositions -= 1;
+      }
+    }
 
-		//Add item to list.
-		this.itemList.push(copyUnit(item));
+    //Add item to list.
+    this.itemList.push(copyUnit(item));
 
-		return true;
-	};
+    return true;
+  };
 
-	/* Container.isLocked(item)
-	 *	Checks if the item is in a locked spot
-	 */
-	this.IsLocked = function (item, baseRef) {
-		var h, w, reference;
+  /* Container.isLocked(item)
+   *	Checks if the item is in a locked spot
+   */
+  this.IsLocked = function (item, baseRef) {
+    var h, w, reference;
 
-		reference = baseRef.slice(0);
+    reference = baseRef.slice(0);
 
-		//Make sure it is in this container.
-		if (item.mode !== 0 || item.location !== this.location) {
-			return false;
-		}
+    //Make sure it is in this container.
+    if (item.mode !== 0 || item.location !== this.location) {
+      return false;
+    }
 
-		// Make sure the item is ours
-		if (!item.getParent() || item.getParent().type !== me.type || item.getParent().gid !== me.gid) {
-			return false;
-		}
+    // Make sure the item is ours
+    if (
+      !item.getParent() ||
+      item.getParent().type !== me.type ||
+      item.getParent().gid !== me.gid
+    ) {
+      return false;
+    }
 
-		//Insure valid reference.
-		if (typeof (reference) !== "object" || reference.length !== this.buffer.length || reference[0].length !== this.buffer[0].length) {
-			throw new Error("Storage.IsLocked: Invalid inventory reference");
-		}
+    //Insure valid reference.
+    if (
+      typeof reference !== "object" ||
+      reference.length !== this.buffer.length ||
+      reference[0].length !== this.buffer[0].length
+    ) {
+      throw new Error("Storage.IsLocked: Invalid inventory reference");
+    }
 
-		try {
-			// Check if the item lies in a locked spot.
-			for (h = item.y; h < (item.y + item.sizey); h += 1) {
-				for (w = item.x; w < (item.x + item.sizex); w += 1) {
-					if (reference[h][w] === 0) {
-						return true;
-					}
-				}
-			}
-		} catch (e2) {
-			throw new Error("Storage.IsLocked error! Item info: " + item.name + " " + item.y + " " + item.sizey + " " + item.x + " " + item.sizex + " " + item.mode + " " + item.location);
-		}
+    try {
+      // Check if the item lies in a locked spot.
+      for (h = item.y; h < item.y + item.sizey; h += 1) {
+        for (w = item.x; w < item.x + item.sizex; w += 1) {
+          if (reference[h][w] === 0) {
+            return true;
+          }
+        }
+      }
+    } catch (e2) {
+      throw new Error(
+        "Storage.IsLocked error! Item info: " +
+          item.name +
+          " " +
+          item.y +
+          " " +
+          item.sizey +
+          " " +
+          item.x +
+          " " +
+          item.sizex +
+          " " +
+          item.mode +
+          " " +
+          item.location
+      );
+    }
 
-		return false;
-	};
+    return false;
+  };
 
-	this.Reset = function () {
-		var h, w;
+  this.Reset = function () {
+    var h, w;
 
-		for (h = 0; h < this.height; h += 1) {
-			for (w = 0; w < this.width; w += 1) {
-				this.buffer[h][w] = 0;
-			}
-		}
+    for (h = 0; h < this.height; h += 1) {
+      for (w = 0; w < this.width; w += 1) {
+        this.buffer[h][w] = 0;
+      }
+    }
 
-		this.itemList = [];
-		this.openPositions = this.height * this.width;
-		return true;
-	};
+    this.itemList = [];
+    this.openPositions = this.height * this.width;
+    return true;
+  };
 
-	/* Container.CanFit(item)
-	 *	Checks to see if we can fit the item in the buffer.
-	 */
-	this.CanFit = function (item) {
-		return (!!this.FindSpot(item));
-	};
+  /* Container.CanFit(item)
+   *	Checks to see if we can fit the item in the buffer.
+   */
+  this.CanFit = function (item) {
+    return !!this.FindSpot(item);
+  };
 
-	/* Container.FindSpot(item)
-	 *	Finds a spot available in the buffer to place the item.
-	 */
-	this.FindSpot = function (item) {
-		var x, y, nx, ny;
+  /* Container.FindSpot(item)
+   *	Finds a spot available in the buffer to place the item.
+   */
+  this.FindSpot = function (item) {
+    var x, y, nx, ny;
 
-		//Make sure it's a valid item
-		if (!item) {
-			return false;
-		}
+    //Make sure it's a valid item
+    if (!item) {
+      return false;
+    }
 
-		Storage.Reload();
+    Storage.Reload();
 
-		//Loop buffer looking for spot to place item.
-		for (y = 0; y < this.width - (item.sizex - 1); y += 1) {
-Loop:
-			for (x = 0; x < this.height - (item.sizey - 1); x += 1) {
-				//Check if there is something in this spot.
-				if (this.buffer[x][y] > 0) {
-					continue;
-				}
+    //Loop buffer looking for spot to place item.
+    for (y = 0; y < this.width - (item.sizex - 1); y += 1) {
+      Loop: for (x = 0; x < this.height - (item.sizey - 1); x += 1) {
+        //Check if there is something in this spot.
+        if (this.buffer[x][y] > 0) {
+          continue;
+        }
 
-				//Loop the item size to make sure we can fit it.
-				for (nx = 0; nx < item.sizey; nx += 1) {
-					for (ny = 0; ny < item.sizex; ny += 1) {
-						if (this.buffer[x + nx][y + ny]) {
-							continue Loop;
-						}
-					}
-				}
+        //Loop the item size to make sure we can fit it.
+        for (nx = 0; nx < item.sizey; nx += 1) {
+          for (ny = 0; ny < item.sizex; ny += 1) {
+            if (this.buffer[x + nx][y + ny]) {
+              continue Loop;
+            }
+          }
+        }
 
-				return ({x: x, y: y});
-			}
-		}
+        return { x: x, y: y };
+      }
+    }
 
-		return false;
-	};
+    return false;
+  };
 
-	/* Container.MoveTo(item)
-	 *	Takes any item and moves it into given buffer.
-	 */
-	this.MoveTo = function (item) {
-		var nPos, n, nDelay, cItem, cube;
+  /* Container.MoveTo(item)
+   *	Takes any item and moves it into given buffer.
+   */
+  this.MoveTo = function (item) {
+    var nPos, n, nDelay, cItem, cube;
 
-		try {
-			//Can we even fit it in here?
-			nPos = this.FindSpot(item);
+    try {
+      //Can we even fit it in here?
+      nPos = this.FindSpot(item);
 
-			if (!nPos) {
-				return false;
-			}
+      if (!nPos) {
+        return false;
+      }
 
-			//Cube -> Stash, must place item in inventory first
-			if (item.location === 6 && this.location === 7 && !Storage.Inventory.MoveTo(item)) {
-				return false;
-			}
+      //Cube -> Stash, must place item in inventory first
+      if (
+        item.location === 6 &&
+        this.location === 7 &&
+        !Storage.Inventory.MoveTo(item)
+      ) {
+        return false;
+      }
 
-			//Can't deal with items on ground!
-			if (item.mode === 3) {
-				return false;
-			}
+      //Can't deal with items on ground!
+      if (item.mode === 3) {
+        return false;
+      }
 
-			//Item already on the cursor.
-			if (me.itemoncursor && item.mode !== 4) {
-				return false;
-			}
+      //Item already on the cursor.
+      if (me.itemoncursor && item.mode !== 4) {
+        return false;
+      }
 
-			//Make sure stash is open
-			if (this.location === 7 && !Town.openStash()) {
-				return false;
-			}
+      //Make sure stash is open
+      if (this.location === 7 && !Town.openStash()) {
+        return false;
+      }
 
-			//Pick to cursor if not already.
-			if (!item.toCursor()) {
-				return false;
-			}
+      //Pick to cursor if not already.
+      if (!item.toCursor()) {
+        return false;
+      }
 
-			//Loop three times to try and place it.
-			for (n = 0; n < 5; n += 1) {
-				if (this.location === 6) { // place item into cube
-					cItem = getUnit(100);
-					cube = me.getItem(549);
+      //Loop three times to try and place it.
+      for (n = 0; n < 5; n += 1) {
+        if (this.location === 6) {
+          // place item into cube
+          cItem = getUnit(100);
+          cube = me.getItem(549);
 
-					if (cItem !== null && cube !== null) {
-						sendPacket(1, 0x2a, 4, cItem.gid, 4, cube.gid);
-					}
-				} else if (this.location === 2) {
-					cItem = getUnit(100);
-					if (cItem !== null) {
-						sendPacket(1, 0x23, 4, cItem.gid, 4, nPos.y);
-					}
-				} else {
-					clickItemAndWait(0, nPos.y, nPos.x, this.location);
-				}
+          if (cItem !== null && cube !== null) {
+            sendPacket(1, 0x2a, 4, cItem.gid, 4, cube.gid);
+          }
+        } else if (this.location === 2) {
+          cItem = getUnit(100);
+          if (cItem !== null) {
+            sendPacket(1, 0x23, 4, cItem.gid, 4, nPos.y);
+          }
+        } else {
+          clickItemAndWait(0, nPos.y, nPos.x, this.location);
+        }
 
-				nDelay = getTickCount();
+        nDelay = getTickCount();
 
-				while ((getTickCount() - nDelay) < Math.max(1000, me.ping * 3 + 500)) {
-					if (!me.itemoncursor) {
-						print("Successfully placed " + item.name + " at X: " + nPos.x + " Y: " + nPos.y);
-						delay(200);
+        while (getTickCount() - nDelay < Math.max(1000, me.ping * 3 + 500)) {
+          if (!me.itemoncursor) {
+            print(
+              "Successfully placed " +
+                item.name +
+                " at X: " +
+                nPos.x +
+                " Y: " +
+                nPos.y
+            );
+            delay(200);
 
-						return true;
-					}
+            return true;
+          }
 
-					delay(10);
-				}
-			}
+          delay(10);
+        }
+      }
 
-			return true;
-		} catch (e) {
-			return false;
-		}
-	};
+      return true;
+    } catch (e) {
+      return false;
+    }
+  };
 
-	/* Container.Dump()
-	 *	Prints all known information about container.
-	 */
-	this.Dump = function () {
-		var x, y, string;
+  /* Container.Dump()
+   *	Prints all known information about container.
+   */
+  this.Dump = function () {
+    var x, y, string;
 
-		print(this.name + " has the width of " + this.width + " and the height of " + this.height);
-		print(this.name + " has " + this.itemList.length + " items inside, and has " + this.openPositions + " spots left.");
+    print(
+      this.name +
+        " has the width of " +
+        this.width +
+        " and the height of " +
+        this.height
+    );
+    print(
+      this.name +
+        " has " +
+        this.itemList.length +
+        " items inside, and has " +
+        this.openPositions +
+        " spots left."
+    );
 
-		for (x = 0; x < this.height; x += 1) {
-			string = "";
+    for (x = 0; x < this.height; x += 1) {
+      string = "";
 
-			for (y = 0; y < this.width; y += 1) {
-				string += (this.buffer[x][y] > 0) ? "ÿc1x" : "ÿc0o";
-			}
+      for (y = 0; y < this.width; y += 1) {
+        string += this.buffer[x][y] > 0 ? "ÿc1x" : "ÿc0o";
+      }
 
-			print(string);
-		}
-	};
+      print(string);
+    }
+  };
 
-	/* Container.UsedSpacePercent()
-	 *	Returns percentage of the container used.
-	 */
-	this.UsedSpacePercent = function () {
-		var x, y,
-			usedSpace = 0,
-			totalSpace = this.height * this.width;
+  /* Container.UsedSpacePercent()
+   *	Returns percentage of the container used.
+   */
+  this.UsedSpacePercent = function () {
+    var x,
+      y,
+      usedSpace = 0,
+      totalSpace = this.height * this.width;
 
-		Storage.Reload();
+    Storage.Reload();
 
-		for (x = 0; x < this.height; x += 1) {
-			for (y = 0; y < this.width; y += 1) {
-				if (this.buffer[x][y] > 0) {
-					usedSpace += 1;
-				}
-			}
-		}
+    for (x = 0; x < this.height; x += 1) {
+      for (y = 0; y < this.width; y += 1) {
+        if (this.buffer[x][y] > 0) {
+          usedSpace += 1;
+        }
+      }
+    }
 
-		return usedSpace * 100 / totalSpace;
-	};
+    return (usedSpace * 100) / totalSpace;
+  };
 
-	/* Container.compare(reference)
-	 *	Compare given container versus the current one, return all new items in current buffer.
-	 */
-	this.Compare = function (baseRef) {
-		var h, w, n, item, itemList, reference;
+  /* Container.compare(reference)
+   *	Compare given container versus the current one, return all new items in current buffer.
+   */
+  this.Compare = function (baseRef) {
+    var h, w, n, item, itemList, reference;
 
-		Storage.Reload();
+    Storage.Reload();
 
-		try {
-			itemList = [];
-			reference = baseRef.slice(0, baseRef.length);
+    try {
+      itemList = [];
+      reference = baseRef.slice(0, baseRef.length);
 
-			//Insure valid reference.
-			if (typeof (reference) !== "object" || reference.length !== this.buffer.length || reference[0].length !== this.buffer[0].length) {
-				throw new Error("Unable to compare different containers.");
-			}
+      //Insure valid reference.
+      if (
+        typeof reference !== "object" ||
+        reference.length !== this.buffer.length ||
+        reference[0].length !== this.buffer[0].length
+      ) {
+        throw new Error("Unable to compare different containers.");
+      }
 
-			for (h = 0; h < this.height; h += 1) {
-Loop:
-				for (w = 0; w < this.width; w += 1) {
-					item = this.itemList[this.buffer[h][w] - 1];
+      for (h = 0; h < this.height; h += 1) {
+        Loop: for (w = 0; w < this.width; w += 1) {
+          item = this.itemList[this.buffer[h][w] - 1];
 
-					if (!item) {
-						continue;
-					}
+          if (!item) {
+            continue;
+          }
 
-					for (n = 0; n < itemList.length; n += 1) {
-						if (itemList[n].gid === item.gid) {
-							continue Loop;
-						}
-					}
+          for (n = 0; n < itemList.length; n += 1) {
+            if (itemList[n].gid === item.gid) {
+              continue Loop;
+            }
+          }
 
-					//Check if the buffers changed and the current buffer has an item there.
-					if (this.buffer[h][w] > 0 && reference[h][w] > 0) {
-						itemList.push(copyUnit(item));
-					}
-				}
-			}
+          //Check if the buffers changed and the current buffer has an item there.
+          if (this.buffer[h][w] > 0 && reference[h][w] > 0) {
+            itemList.push(copyUnit(item));
+          }
+        }
+      }
 
-			return itemList;
-		} catch (e) {
-			return false;
-		}
-	};
+      return itemList;
+    } catch (e) {
+      return false;
+    }
+  };
 
-	this.toSource = function () {
-		return this.buffer.toSource();
-	};
+  this.toSource = function () {
+    return this.buffer.toSource();
+  };
 };
 
-var Storage = new function () {
-	this.Init = function () {
-		this.StashY = me.gametype === 0 ? 4 : 8;
-		this.Inventory = new Container("Inventory", 10, 4, 3);
-		this.TradeScreen = new Container("Inventory", 10, 4, 5);
-		this.Stash = new Container("Stash", 6, this.StashY, 7);
-		this.Belt = new Container("Belt", 4 * this.BeltSize(), 1, 2);
-		this.Cube = new Container("Horadric Cube", 3, 4, 6);
-		this.InvRef = [];
+var Storage = new (function () {
+  this.Init = function () {
+    // this.StashY = me.gametype === 0 ? 4 : 8;
+    this.StashY = me.gametype === 0 ? 4 : 10; // 10 because we are using plugy's 10*10 stash
+    this.Inventory = new Container("Inventory", 10, 4, 3);
+    this.TradeScreen = new Container("Inventory", 10, 4, 5);
+    // this.Stash = new Container("Stash", 6, this.StashY, 7);
+    this.Stash = new Container("Stash", 10, this.StashY, 7); // 10 because we are using plugy's 10*10 stash
+    this.Belt = new Container("Belt", 4 * this.BeltSize(), 1, 2);
+    this.Cube = new Container("Horadric Cube", 3, 4, 6);
+    this.InvRef = [];
 
-		this.Reload();
-	};
+    this.Reload();
+  };
 
-	this.BeltSize = function () {
-		var item = me.getItem(-1, 1); // get equipped item
+  this.BeltSize = function () {
+    var item = me.getItem(-1, 1); // get equipped item
 
-		if (!item) { // nothing equipped
-			return 1;
-		}
+    if (!item) {
+      // nothing equipped
+      return 1;
+    }
 
-		do {
-			if (item.bodylocation === 8) { // belt slot
-				switch (item.code) {
-				case "lbl": // sash
-				case "vbl": // light belt
-					return 2;
-				case "mbl": // belt
-				case "tbl": // heavy belt
-					return 3;
-				default: // everything else
-					return 4;
-				}
-			}
-		} while (item.getNext());
+    do {
+      if (item.bodylocation === 8) {
+        // belt slot
+        switch (item.code) {
+          case "lbl": // sash
+          case "vbl": // light belt
+            return 2;
+          case "mbl": // belt
+          case "tbl": // heavy belt
+            return 3;
+          default:
+            // everything else
+            return 4;
+        }
+      }
+    } while (item.getNext());
 
-		return 1; // no belt
-	};
+    return 1; // no belt
+  };
 
-	this.Reload = function () {
-		this.Inventory.Reset();
-		this.Stash.Reset();
-		this.Belt.Reset();
-		this.Cube.Reset();
-		this.TradeScreen.Reset();
+  this.Reload = function () {
+    this.Inventory.Reset();
+    this.Stash.Reset();
+    this.Belt.Reset();
+    this.Cube.Reset();
+    this.TradeScreen.Reset();
 
-		var item = me.getItem();
+    var item = me.getItem();
 
-		if (!item) {
-			return false;
-		}
+    if (!item) {
+      return false;
+    }
 
-		do {
-			switch (item.location) {
-			case 3:
-				this.Inventory.Mark(item);
+    do {
+      switch (item.location) {
+        case 3:
+          this.Inventory.Mark(item);
 
-				break;
-			case 5:
-				this.TradeScreen.Mark(item);
+          break;
+        case 5:
+          this.TradeScreen.Mark(item);
 
-				break;
-			case 2:
-				this.Belt.Mark(item);
+          break;
+        case 2:
+          this.Belt.Mark(item);
 
-				break;
-			case 6:
-				this.Cube.Mark(item);
+          break;
+        case 6:
+          this.Cube.Mark(item);
 
-				break;
-			case 7:
-				this.Stash.Mark(item);
+          break;
+        case 7:
+          this.Stash.Mark(item);
 
-				break;
-			}
-		} while (item.getNext());
+          break;
+      }
+    } while (item.getNext());
 
-		return true;
-	};
-};
+    return true;
+  };
+})();
