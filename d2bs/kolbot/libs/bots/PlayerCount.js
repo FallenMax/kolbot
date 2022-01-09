451c7f5d1e90269ba6ade8bc71c8ef43d101b448
FallenMax <FallenMax@gmail.com>
2022-01-09 19:25:10 +0800

    add playcounts module

---
+++ d2bs/kolbot/libs/bots/PlayerCount.js
@@ -0,0 +1,10 @@
+/**
+ *	@filename	PlayerCount.js
+ *	@author		fallenmax
+ *	@desc	 set player count
+ */
+
+function PlayerCount() {
+  say("/players " + Config.PlayerCount.Count);
+  return true;
+}
